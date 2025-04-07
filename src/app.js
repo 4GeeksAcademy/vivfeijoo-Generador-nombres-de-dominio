8a32a3e825e086cf901d048be4ec1d3b9171971b
Viviana Feijoo <vivfeijoo@gmail.com>
2025-04-07 09:19:02 +0000

    dominios

--- src/app.js
+++ src/app.js
@@ -14,12 +14,19 @@ window.onload = function() {
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
-  const domainList = document.getElementById("domain-List")
+
+
+  const domainList = document.getElementById("domain-list")
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         let domain = pronoun[i] + adj[j] + noun[k] + ".com";
+        
+        let li = document.createElement("li");
+        li.textContent = (i * adj.length * noun.length + j * noun.length + k + 1) + ". " + domain;
+        domainList.appendChild(li);
+        
         console.log((i * adj.length * noun.length + j * noun.length + k + 1) + domain);
       }
     }
